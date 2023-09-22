d2de3d32f5be48cecbdf05c8d6c4914c2a64f469
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2023-09-22 18:12:55 -0500

    clave api

--- src/app/alarma/gps/gps.component.ts
+++ src/app/alarma/gps/gps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-gps',
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: 'CLAVE_API',
+};
